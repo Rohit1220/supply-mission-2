133a787c47af20e79ee13407753212c7fe696c64
Rohit1220 <65952260+Rohit1220@users.noreply.github.com>
2020-06-26 18:10:22 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,15 @@
+class box{
+    constructor(x, y, width, height) {
+        this.body = Bodies.rectangle(x, y, width, height);
+        this.body.shapeColor="red";
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rect(0,0,this.width,this.height);
+        pop();
+      }
+}
